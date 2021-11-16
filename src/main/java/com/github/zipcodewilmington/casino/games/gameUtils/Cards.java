51b72c85710f2bdc78ce467ad2967f3e4151faee
Keerthana Srinivasan <keerthihamsa.06@gmail.com>
2021-11-16 18:31:18 -0500

    change

--- src/main/java/com/github/zipcodewilmington/casino/games/gameUtils/Cards.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/gameUtils/Cards.java
@@ -1,5 +1,7 @@
 package com.github.zipcodewilmington.casino.games.gameUtils;
 
+import org.junit.jupiter.api.Assertions;
+
 import javax.smartcardio.Card;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,11 +10,11 @@ public class Cards {
 
     private Suit suit;
     private Rank rank;
+
     public Cards(Suit suit, Rank rank) {
         this.suit = suit;
         this.rank = rank;
     }
-
 
     public Suit getSuit(){
         return suit;
@@ -22,5 +24,9 @@ public class Cards {
         return rank;
     }
 
+    @Override
+    public String toString() {
+        return this.suit.getGraphic() + this.rank.getPrimaryValue();
+    }
 
 }
