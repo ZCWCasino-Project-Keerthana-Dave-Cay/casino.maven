8300e642580486bf7f249bb6ed463b7effb8c9ad
Keerthana Srinivasan <keerthihamsa.06@gmail.com>
2021-11-17 15:25:32 -0500

    changes

--- src/main/java/com/github/zipcodewilmington/casino/games/gameUtils/Hand.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/gameUtils/Hand.java
@@ -4,18 +4,6 @@ import java.util.*;
 
 public class Hand {
 
-    private List<Cards> cardInHand = new ArrayList<Cards>();
 
-    public void assignCard(Cards card){
-        cardInHand.add(card);
-    }
-
-    public void assignCard(ArrayList<Cards> newCards){
-        cardInHand.addAll(newCards);
-    }
-
-    public Cards getCard(){
-        return cardInHand.get(0);
-    }
 
 }
