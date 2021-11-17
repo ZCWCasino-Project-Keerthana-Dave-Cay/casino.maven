bcda6846074d96cc8b4246b2ebc536ab40bf6da3
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-17 15:34:19 -0500

    fixed hand class by adding back methods

--- src/main/java/com/github/zipcodewilmington/casino/games/gameUtils/Hand.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/gameUtils/Hand.java
@@ -4,14 +4,29 @@ package com.github.zipcodewilmington.casino.games.gameUtils;
 
 public class Hand extends Deck {
 
-
     public Deck dealerBlackJackHand() {
         Deck dealersHandBJ = new Deck();
         dealersHandBJ.drawMultipleCards(2);
         return dealersHandBJ;
     }
 
+    public Deck dealerWarHand() {
+        Deck dealerHandWar = new Deck();
+        dealerHandWar.drawMultipleCards(1);
+        return dealerHandWar;
+    }
+
+    public Deck playerBlackJackHand(){
+        Deck playersHandBJ = new Deck();
+        playersHandBJ.drawMultipleCards(2);
+        return playersHandBJ;
+    }
+
+    public Deck playerWarHand() {
+        Deck playersHandWar = new Deck();
+        playersHandWar.drawMultipleCards(1);
+        return playersHandWar;
 
 
-
+    }
 }
