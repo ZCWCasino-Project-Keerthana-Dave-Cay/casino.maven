5ae7415326c823f3c0706c1730a4f04e62ab162f
David Nguyen <davebox309@gmail.com>
2021-11-18 17:07:46 -0500

    exit the game

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/BigSixWheelEngine.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/BigSixWheelEngine.java
@@ -1,6 +1,6 @@
 package com.github.zipcodewilmington.casino.games.slots;
 
-import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.utils.AnsiColor;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,13 +14,16 @@ public class BigSixWheelEngine {
     private Random tileRandomizer;
     private Map<Integer, Integer> playerBets;
     private Scanner keyboard;
+    private AnsiColor ansiColor;
 
-    public BigSixWheelEngine(BigSixWheel bigSixWheel){
+
+    public BigSixWheelEngine(BigSixWheel bigSixWheel) {
         this.bigSixWheel = bigSixWheel;
         this.tileRandomizer = new Random();
         this.playerBets = new HashMap<>();
         this.keyboard = new Scanner(System.in);
     }
+
 
 
     public void start() {
@@ -29,22 +32,25 @@ public class BigSixWheelEngine {
         boolean isPlayerSettingBet = true;
         int tileNumber;
         while (isPlayerSettingBet) {
-            System.out.print("Enter your Tile Number (-1 to finish): ");
+            System.out.println("Welcome to the Big Six Wheel! \n Pick a tile from 1-7... (-1 to finish): ");
             tileNumber = keyboard.nextInt();
-            if(tileNumber >= bigSixWheel.getPossibleWheelHits().size() || tileNumber < -1){
+            if (tileNumber >= bigSixWheel.getPossibleWheelHits().size() || tileNumber < -1) {
                 System.out.println("Invalid Input!");
-            }
-            else if(tileNumber == -1){
+            } else if (tileNumber == -1) {
                 System.out.println("Player's Bets Finished");
                 isPlayerSettingBet = false;
-            }
-            else{
+            } else {
                 //TODO - set limits on just betting on 7 tiles
                 //TODO - check account balance against player bets
                 //TODO - enforce a min bet to the corresponding tile value
+                //TODO - create an exit method
                 System.out.println("Enter Bet Amount: ");
                 int betAmount = keyboard.nextInt();
-                playerBets.put(tileNumber,playerBets.getOrDefault(tileNumber,0) + betAmount);
+                if (betAmount < 0) { //no negative bets
+                    System.out.println("Invalid Input!");
+                } else {
+                    playerBets.put(tileNumber, playerBets.getOrDefault(tileNumber, 0) + betAmount);
+                }
             }
         }
         //play game
@@ -52,16 +58,33 @@ public class BigSixWheelEngine {
 
         //collect winnings
         payPlayer(results);
+
+        //prompt me if i would like to continue?
+        System.out.println("Would you to continue?");
+        String y = keyboard.next();
+        if( y.equalsIgnoreCase("YES")){
+            start();
+        }
+
     }
 
-    public void payPlayer(int winningTile){
+
+    public void payPlayer(int winningTile) {
         int payOut = 0;
-        if(playerBets.getOrDefault(winningTile,0) > 0){
+        if (playerBets.getOrDefault(winningTile, 0) > 0) {
             payOut += playerBets.get(winningTile) * bigSixWheel.getPossibleWheelHits().get(winningTile);
+            System.out.println("Paying Player $" + payOut + " in Winnings ");
         }
-        System.out.println("Paying Player $" + payOut + " in Winnings ");
+        else{
+            System.out.println("You did not win!");
+            System.out.println("Paying Player $" + payOut);
+        }
         bigSixWheel.getPlayer().getArcadeAccount().addWinningsToBalance(payOut);
+        //loop back to pick your tile instead of exiting the game
+
     }
+
+
 
     public void reset(){
         playerBets.clear();
